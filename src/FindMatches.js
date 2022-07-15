c3760f9549d09e5acd77c946b256202bef3dc606
Giovanny Sayas <gsayas@gmail.com>
2022-07-15 17:14:49 +0200

    adds helloworld function

---
+++ src/FindMatches.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class FindMatches extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };       
+    }
+
+    render() {
+        return <div>Tribu matching!</div>
+    }
+
+}
+
+export default FindMatches
