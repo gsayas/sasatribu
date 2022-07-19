abe9a887ea3af65cf9fea41242ecbc8cdaaee489
Giovanny Sayas <gsayas@gmail.com>
2022-07-19 18:10:04 +0200

    adds atomize theme

--- src/components/FindMatches.js
+++ src/components/FindMatches.js
@@ -2,6 +2,12 @@ import './FindMatches.css';
 import { useEffect, useState } from 'react';
 import Hammer from 'hammerjs'
 import data from '../mocks/matches.json';
+import {
+    ThemeProvider,
+    DefaultTheme,
+    StyleReset,
+    Button,
+  } from "react-atomize";
 
 console.log(data)
 
@@ -11,15 +17,15 @@ const FindMatches = () => {
 
     useEffect(() => {
         const currentMatchContainer = document.querySelector('.current-match');
-        
-        const mc = new Hammer(currentMatchContainer);
-        mc.on("panleft panright", function(ev) {
 
-            if(ev.type == 'panleft'){
+        const mc = new Hammer(currentMatchContainer);
+        mc.on("panleft panright", function (ev) {
+
+            if (ev.type == 'panleft') {
                 console.log('rejected!')
             }
 
-            if(ev.type == 'panright'){
+            if (ev.type == 'panright') {
                 console.log('liked!')
             }
 
@@ -29,25 +35,43 @@ const FindMatches = () => {
 
     const renderMatch = (match) => {
         return <div key={match.id} className='match-card'>
-                    <pre>
-                        <img src={match.picture} />
-                        <span>{match.name}</span>
-                        <span>{match.age}</span>
-                    </pre>
-                </div>
+            <pre>
+                <img src={match.picture} />
+                <span>{match.name}</span>
+                <span>{match.age}</span>
+            </pre>
+        </div>
     }
 
     const showNextMatch = () => {
-        if(currentMatch != matches.length-1){
+        if (currentMatch != matches.length - 1) {
             setCurrentMatch(currentMatch + 1)
-        }else{
+        } else {
             setCurrentMatch(0)
         }
     }
 
+    const theme = {
+        ...DefaultTheme,
+        colors: {
+            ...DefaultTheme.colors,
+            brand800: "#671de1"
+        },
+        rounded: {
+            ...DefaultTheme.rounded,
+            brandRadius: "20px"
+        }
+    };
+
     return <div className='find-matches'>
-        <h1>Tribu matching!</h1>
-        <div className='current-match'>{renderMatch(matches.at(currentMatch))}</div>
+        <ThemeProvider theme={theme}>
+            <StyleReset />
+            <h1>Tribu matching!</h1>
+            <div className='current-match'>{renderMatch(matches.at(currentMatch))}</div>
+            <Button bg="brand800" rounded="brandRadius">
+                Submit
+            </Button>
+        </ThemeProvider>
     </div>
 
 }
